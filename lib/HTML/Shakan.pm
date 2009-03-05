670cbad551c659e0f492723c9b0f2c9b5769ba93
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-05 10:13:16 +0900

    initial commit

---
+++ lib/HTML/Shakan.pm
@@ -0,0 +1,33 @@
+package HTML::Shakan;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::Shakan -
+
+=head1 SYNOPSIS
+
+  use HTML::Shakan;
+
+=head1 DESCRIPTION
+
+HTML::Shakan is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
