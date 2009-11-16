35e003197d06b0c6d7b5a62ea03d7efa4a549c18
gfx <gfuji@cpan.org>
2009-11-16 21:29:02 +0900

    Remove a wrong use of weak_ref => 1

--- lib/HTML/Shakan.pm
+++ lib/HTML/Shakan.pm
@@ -30,7 +30,6 @@ has '_fvl' => (
     isa => 'FormValidator::Lite',
     lazy => 1,
     handles => [qw/has_error load_function_message get_error_messages is_error is_valid set_error set_message/],
-    weak_ref => 1,
     default => sub {
         my $self = shift;
         $self->params(); # build laziness data
