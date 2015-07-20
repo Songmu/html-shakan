3803511ae48ad615a9bf7ad8b76141dadab28b90
Songmu <y.songmu@gmail.com>
2015-07-21 00:12:43 +0900

    suppress CGI list context warnings in testing

--- t/Util.pm
+++ t/Util.pm
@@ -4,6 +4,19 @@ use warnings;
 use base 'Exporter';
 use CGI;
 
+BEGIN {
+    # XXX suppress CGI list context warnings.
+    # This is only workaround. We should remove CGI.pm dependency from testing and switch Plack::Request or so.
+    my $orig = CGI->can('param');
+    no warnings 'redefine';
+    no strict 'refs';
+    *CGI::param = sub {
+        use strict 'refs';
+        local $CGI::LIST_CONTEXT_WARN = 0;
+        $orig->(@_);
+    };
+}
+
 our @EXPORT = qw(query trim);
 
 sub query { CGI->new(@_) }
